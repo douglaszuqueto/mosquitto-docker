e74ec0656bbf0c1accf560893ca0b305c26266c2
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2018-06-11 06:11:01 -0300

    Montagem da estrutura base

--- build.sh
+++ build.sh
@@ -1,7 +1,77 @@
 #!/usr/bin/env bash
 
-docker build -t mosquitto-test . --no-cache
+# if there is a env file, source it
+if [ -f "./.env" ]; then
+   source ./.env
+# source example else
+else
+   source ./.env.example
+fi
 
-docker images mosquitto-test
+# enabled repositories for the build
+REPOSITORIES=$1
 
-docker run -it --rm --name mosquitto-test -p 1883:1883 -p 8083:8083 mosquitto-test
+
+# enable all repositories if any specified
+if [[ -z $REPOSITORIES ]]; then
+    REPOSITORIES="mosquitto"
+fi
+
+# for returning later to the main directory
+ROOT_DIRECTORY=`pwd`
+
+# function for building images
+function build_repository {
+    # read repository configuration
+    source $ROOT_DIRECTORY/$REPOSITORY/buildvars
+
+    # build all enabled versions
+    for TAG in $TAGS; do
+      # some verbose
+      echo $'\n\n'"--> Building $NAMESPACE/$REPOSITORY:$TAG"$'\n'
+      cd $ROOT_DIRECTORY/$REPOSITORY/$TAG
+
+      if [ $USE_CACHE == true ]; then
+        # build using cache
+        docker build -t $NAMESPACE/$REPOSITORY:$TAG .
+      fi
+
+      if [ $USE_CACHE == false ]; then
+        docker build --no-cache -t $NAMESPACE/$REPOSITORY:$TAG .
+      fi
+    done
+
+    # create the latest tag
+    echo $'\n\n'"--> Aliasing $LATEST as 'latest'"$'\n'
+    docker tag $NAMESPACE/$REPOSITORY:$LATEST $NAMESPACE/$REPOSITORY:latest
+}
+
+# function for publishing images
+function publish_repository {
+    # read repository configuration
+    source $ROOT_DIRECTORY/$REPOSITORY/buildvars
+
+    # publish all enabled versions
+    for TAG in $TAGS; do
+      # some verbose
+      echo $'\n\n'"--> Publishing $NAMESPACE/$REPOSITORY:$TAG"$'\n'
+      # publish
+      docker push $NAMESPACE/$REPOSITORY:$TAG
+    done
+
+    # create the latest tag
+    echo $'\n\n'"--> Publishing $NAMESPACE/$REPOSITORY:latest (from $LATEST)"$'\n'
+    docker push $NAMESPACE/$REPOSITORY:latest
+}
+
+# for each enabled repository
+for REPOSITORY in $REPOSITORIES; do
+  # build the repository
+  build_repository $REPOSITORY
+
+  # If publishing is enabled
+  if [ $PUBLISH == true ]; then
+    # Push the built image
+    publish_repository $REPOSITORY
+  fi
+done
