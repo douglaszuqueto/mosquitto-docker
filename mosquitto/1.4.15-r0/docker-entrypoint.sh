af545866f8079fcb663de8e456fb4b9855bb24a4
Douglas Zuqueto <douglas.zuqueto@gmail.com>
2018-06-12 07:55:50 -0300

    Adicionando teste de environment

--- mosquitto/1.4.15-r0/docker-entrypoint.sh
+++ mosquitto/1.4.15-r0/docker-entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/ash
 
 set -e
+
+chown -R mosquitto:mosquitto /mosquitto
+
+CONFIG=/mosquitto/config/mosquitto.conf
+
+if [[ -v 'MOSQUITTO_PERSISTENCE' ]]; then
+    if [ $MOSQUITTO_PERSISTENCE == true ] || [ $MOSQUITTO_PERSISTENCE == false ]; then
+        sed -r -i "s/(^#*)(persistence)\s+(.*)/\2 $(eval echo \$persistence $MOSQUITTO_PERSISTENCE|sed -e 's/\//\\\//g')/g" $CONFIG
+    fi
+fi
+
 exec "$@"
